544a163741bf15d9897c4239dec2059b93535e87
dunjadjordjevic <dunjadjordjevic97@gmail.com>
2022-07-09 12:16:35 +0200

    Project structure changes

--- tyreoid_project/asgi.py
+++ tyreoid_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for tyreoidWebapp project.
+ASGI config for tyreoid_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tyreoidWebapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tyreoid_project.settings')
 
 application = get_asgi_application()
