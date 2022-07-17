414012dd86f3fd433156f7e57d1f146797816a62
dunjadjordjevic <dunjadjordjevic97@gmail.com>
2022-07-17 23:38:38 +0200

    Tuning of hyperparameters for Random forest algorithm added

--- tyreoid_project/main.py
+++ tyreoid_project/main.py
@@ -4,7 +4,11 @@ import pandas as pd
 import numpy as np
 from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
 from models import evaluator
+from tyreoid_project.models.plot_functions import create_plot
+
 
 def clear_dataset(thyroidDataset):
 
@@ -159,10 +163,48 @@ y = thyroidDataset['target']
 x = thyroidDataset.drop('target', axis=1)
 
 #20% test, 80% train
-x_train, x_test, y_train, y_test= train_test_split(x, y, test_size=0.2)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 number_of_records_in_training_set = len(x_train)
 number_of_records_in_test_set = len(x_test)
+
+# TODO: For plotting we need to specify columns from dataframe which we want to plot
+#create_plot(pd.concat([x_train, y_train], axis='columns'), thyroidDataset.columns)
+
+# Check best parameters for Random forest algorithm
+'''
+    number_of_trees,
+    number_of_data_in_bootstrap_dataset,
+    number_of_features,
+    max_depth
+'''
+
+'''
+print('\nCalculating best parameters for Random forest algorithm...')
+
+def display(results):
+    print(f'Best parameters are: {results.best_params_}')
+    print("\n")
+    mean_score = results.cv_results_['mean_test_score']
+    std_score = results.cv_results_['std_test_score']
+    params = results.cv_results_['params']
+    for mean,std,params in zip(mean_score,std_score,params):
+        print(f'{round(mean,3)} + or -{round(std,3)} for the {params}')
+
+
+rfc = RandomForestClassifier()
+parameters = {
+    "bootstrap": [True],
+    "min_samples_split": [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22],
+    "n_estimators": [1, 5, 10, 20, 50],
+    "max_depth": [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+}
+cv = GridSearchCV(rfc, parameters, cv=10)
+cv.fit(x_train, y_train.values.ravel())
+#print(cv.best_params_)
+display(cv)
+'''
 
 evaluator = evaluator.Evaluator()
 evaluator.evaluate(pd.concat([x_train, y_train], axis='columns'), thyroidDataset.columns)
