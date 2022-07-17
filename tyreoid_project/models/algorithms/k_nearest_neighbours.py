f6f3467a824f9ce2a8a3c8aed9a742a8d86e4163
dunjadjordjevic <dunjadjordjevic97@gmail.com>
2022-07-17 23:36:38 +0200

    Implementation of KNN moved to new file

---
+++ tyreoid_project/models/algorithms/k_nearest_neighbours.py
@@ -0,0 +1,50 @@
+import math
+
+def euclidean_distance(x, y):
+    distance = 0.0
+    for i in range(len(x) - 1):
+        distance += (x[i] - y[i]) ** 2
+
+    return math.sqrt(distance)
+
+
+def get_neighbours(train_set, test_entry, number_of_neighbors):
+
+    # find distance from every neighbour in train_set, and then sort them by distance ASC
+    # get first number_of_neighbours from the top
+    list_of_neighbours = list()
+    distances = list()
+
+    # distance will be calculated with Euclidean distance formula
+    for train_entry in train_set:
+        distance_from_neighbour = euclidean_distance(train_entry, test_entry)
+        distances.append((train_entry, distance_from_neighbour))
+
+    distances.sort(key=lambda d: d[1])
+
+    for i in range(number_of_neighbors):
+        list_of_neighbours.append(distances[i][0])
+
+    return list_of_neighbours
+
+
+def predict_classification(train_set, test_row, number_of_neighbors):
+
+    list_of_neighbours = get_neighbours(train_set, test_row, number_of_neighbors)
+    target_neighbours_values = [entry[-1] for entry in list_of_neighbours]
+    prediction_value = max(set(target_neighbours_values), key=target_neighbours_values.count)
+
+    return prediction_value
+
+
+def k_nearest_neighbors(train_set, test_set, columns, number_of_neighbors):
+
+    prediction_values = list()
+    for entry in test_set:
+        prediction_value = predict_classification(train_set, entry, number_of_neighbors)
+        prediction_values.append(prediction_value)
+
+    print('Prediction values: ')
+    print(prediction_values)
+
+    return prediction_values
