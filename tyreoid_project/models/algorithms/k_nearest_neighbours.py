3c6e254eb41bdd5d92f405c1137a95139c1aa09c
dunjadjordjevic <dunjadjordjevic97@gmail.com>
2022-07-26 19:25:31 +0200

    Evaluator update
    Improve results of evaluation with calculating precision, recall, f1score besides accuracy.
    
    Determine regular coefficients for KNN and use it to support weighting of attributes.
    
    Predict target classes for new patients from test set for every used algorithm.

--- tyreoid_project/models/algorithms/k_nearest_neighbours.py
+++ tyreoid_project/models/algorithms/k_nearest_neighbours.py
@@ -1,14 +1,15 @@
 import math
 
-def euclidean_distance(x, y):
+def euclidean_distance(x, y, w):
+
     distance = 0.0
     for i in range(len(x) - 1):
-        distance += (x[i] - y[i]) ** 2
+        distance += (w[i]*(x[i] - y[i])) ** 2
 
     return math.sqrt(distance)
 
 
-def get_neighbours(train_set, test_entry, number_of_neighbors):
+def get_neighbours(train_set, test_entry, number_of_neighbors, w):
 
     # find distance from every neighbour in train_set, and then sort them by distance ASC
     # get first number_of_neighbours from the top
@@ -17,7 +18,7 @@ def get_neighbours(train_set, test_entry, number_of_neighbors):
 
     # distance will be calculated with Euclidean distance formula
     for train_entry in train_set:
-        distance_from_neighbour = euclidean_distance(train_entry, test_entry)
+        distance_from_neighbour = euclidean_distance(train_entry, test_entry, w)
         distances.append((train_entry, distance_from_neighbour))
 
     distances.sort(key=lambda d: d[1])
@@ -28,20 +29,19 @@ def get_neighbours(train_set, test_entry, number_of_neighbors):
     return list_of_neighbours
 
 
-def predict_classification(train_set, test_row, number_of_neighbors):
+def predict_classification(train_set, test_row, number_of_neighbors, w):
 
-    list_of_neighbours = get_neighbours(train_set, test_row, number_of_neighbors)
+    list_of_neighbours = get_neighbours(train_set, test_row, number_of_neighbors, w)
     target_neighbours_values = [entry[-1] for entry in list_of_neighbours]
     prediction_value = max(set(target_neighbours_values), key=target_neighbours_values.count)
 
     return prediction_value
 
-
-def k_nearest_neighbors(train_set, test_set, columns, number_of_neighbors):
+def k_nearest_neighbors(train_set, test_set, columns, number_of_neighbors, w):
 
     prediction_values = list()
     for entry in test_set:
-        prediction_value = predict_classification(train_set, entry, number_of_neighbors)
+        prediction_value = predict_classification(train_set, entry, number_of_neighbors, w)
         prediction_values.append(prediction_value)
 
     print('Prediction values: ')
--- tyreoid_project/models/evaluator.py
+++ tyreoid_project/models/evaluator.py
@@ -1,7 +1,21 @@
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
 from random import randrange
 from tyreoid_project.models.algorithms.decision_tree import *
 from tyreoid_project.models.algorithms.k_nearest_neighbours import *
 from tyreoid_project.models.algorithms.random_forest import *
+from tyreoid_project.models.algorithms.naive_bayes_classifier import *
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+
+def print_metrics(scores, precision, recall, f1score):
+
+    print('Scores: %s' % scores)
+    print('Mean Accuracy: %.3f%%' % (sum(scores) / float(len(scores))))
+    print('Precision: %.3f%%' % (sum(precision) / float(len(precision))))
+    print('Recall: %.3f%%' % (sum(recall) / float(len(recall))))
+    print('F1 score: %.3f%%' % (sum(f1score) / float(len(f1score))))
+
 
 class Evaluator:
 
@@ -10,6 +24,9 @@ class Evaluator:
     number_of_records_in_training_set = 0
 
     def accuracy_metric(self, actual_values, prediction_values):
+
+        actual_values = list(map(int, actual_values))
+        prediction_values = list(map(int, prediction_values))
 
         number_of_correct_predictions = 0
         for i in range(len(actual_values)):
@@ -34,15 +51,28 @@ class Evaluator:
 
         return folds
 
+    ### Function for calculation weights for attributes ####
+    def regularization_coeffients(self, X, target, lambd=1):
+
+        reg_coefficients = []
+        for row in X.transpose().iterrows():
+            r = abs(pearsonr(list(row[1]), target)[0]) ** lambd
+            reg_coefficients.append(r)
+
+        reg_coefficients = [1 if math.isnan(x) else x for x in reg_coefficients]
+
+        return reg_coefficients
+
+
     #### Wrappers for calling algorithm implementation methods ###
 
-    def k_nearest_neigbours_call(self, train_set, test_set, columns, number_of_neighbors):
+    def k_nearest_neigbours_call(self, train_set, test_set, columns, number_of_neighbors, w=[1]*21):
 
-        return k_nearest_neighbors(train_set, test_set, columns, number_of_neighbors)
+        return k_nearest_neighbors(train_set, test_set, columns, number_of_neighbors, w)
 
-    def decision_tree_algorithm_call(self, train_set, test_set, columns, min_sample, max_depth):
+    def decision_tree_algorithm_call(self, train_set, test_set, columns, min_sample, max_depth, metric_function):
 
-        return classification_decision_tree(train_set, test_set, columns, min_sample, max_depth)
+        return classification_decision_tree(train_set, test_set, columns, min_sample, max_depth, metric_function)
 
     def random_forest_algorithm_call(self,
                                      train_set,
@@ -51,18 +81,28 @@ class Evaluator:
                                      number_of_trees,
                                      number_of_data_in_bootstrap_dataset,
                                      number_of_features,
-                                     max_depth):
+                                     max_depth,
+                                     metric_function):
         return random_forest(train_set,
                              test_set,
                              columns,
                              number_of_trees,
                              number_of_data_in_bootstrap_dataset,
                              number_of_features,
-                             max_depth)
+                             max_depth,
+                             metric_function)
+
+    def naive_bayes_classifier_call(self,  train_set, test_set, columns):
+
+        return naive_bayes_classifier(self, train_set, test_set, columns)
 
     def evaluate_algorithm(self, dataset, columns, algorithm, *args):
 
         scores = list()
+        precision = list()
+        recall = list()
+        f1score = list()
+
         folds = self.cross_validation_split(dataset)
 
         for idx, fold in enumerate(folds):
@@ -74,15 +114,53 @@ class Evaluator:
             for entry_idx, entry in enumerate(fold):
                 entry_tmp = list(entry)
                 test_fold.append(entry_tmp)
-                entry_tmp[-1] = None
+                #entry_tmp[-1] = None
 
             print('Calling algorithm for test fold with index: ', idx)
             prediction_values = algorithm(train_folds, test_fold, columns, *args)
+            prediction_values = [int(x) for x in prediction_values]
             actual_values = [entry[-1] for entry in fold]
+            actual_values = [int(x) for x in actual_values]
             accuracy_metric = self.accuracy_metric(actual_values, prediction_values)
             scores.append(accuracy_metric)
+            precision.append(precision_score(actual_values, prediction_values, average='macro'))
+            recall.append(recall_score(actual_values, prediction_values, average='macro'))
+            f1score.append(f1_score(actual_values, prediction_values, average='macro'))
 
-        return scores
+        print(scores)
+        return scores, precision, recall, f1score
+
+    def predict_algorithm(self, train_set, test_set, columns, algorithm_name, *args):
+
+        train_set = list(train_set.values.tolist())
+        test_set = list(test_set.values.tolist())
+
+        if (algorithm_name == 'k_nearest_neighbours'):
+            algorithm = self.k_nearest_neigbours_call
+        elif(algorithm_name == 'decision_tree_algorithm'):
+            algorithm = self.decision_tree_algorithm_call
+        elif (algorithm_name == 'random_forest_algorithm'):
+            algorithm = self.random_forest_algorithm_call
+        elif (algorithm_name == 'naive_bayes_classifier_algorithm'):
+            algorithm = self.naive_bayes_classifier_call
+
+
+        print('--> Starting with prediction on test set <--')
+        prediction_values = algorithm(train_set, test_set, columns, *args)
+        prediction_values = [int(x) for x in prediction_values]
+        actual_values = [entry[-1] for entry in test_set]
+        actual_values = [int(x) for x in actual_values]
+        accuracy_metric = self.accuracy_metric(actual_values, prediction_values)
+        precision = precision_score(actual_values, prediction_values, average='macro')
+        recall = recall_score(actual_values, prediction_values, average='macro')
+        f1score = f1_score(actual_values, prediction_values, average='macro')
+        print('Mean Accuracy for prediction is : %.3f%%' % accuracy_metric)
+        print('Precision for prediction is : %.3f%%' % precision)
+        print('Recall for prediction is : %.3f%%' % recall)
+        print('F1 score for prediction is : %.3f%%' % f1score)
+        print('--> End of prediction on test set <--')
+
+        return prediction_values
 
     def evaluate(self, dataset, columns):
 
@@ -91,49 +169,113 @@ class Evaluator:
         # Should iterrate over k from k_min to k_max, and find best mean of scores
 
 
-
         self.number_of_records_in_training_set = len(dataset)
-        #file = open('resources/k_indexes.txt', 'w')
 
-        '''
-        self.k_min = 1
+        self.k_min = 3
         self.k_max = math.sqrt((self.number_of_records_in_training_set - self.fold_size) * 0.8)
 
         if (self.k_max % 2) == 0:
             self.k_max = self.k_max + 1
 
+        k_values = []
+        accuracy_values = []
+
+        y_train = dataset['target'].astype(int)
+        x_train = dataset.drop('target', axis=1)
+
+        w = self.regularization_coeffients(x_train, y_train.values)
+        f = open("resources/k_values.txt", "w")
 
         print(" --> START of evaluation of KNN algorithm <-- \n")
-        for k_index in range(self.k_min, int(self.k_max)):
+        #for k_index in range(self.k_min, int(self.k_max), 10):
+        for k_index in range(self.k_min, 130, 10):
+            if (k_index % 2) == 0:
+                continue
+
             print('K index is: ', k_index)
-            scores = self.evaluate_algorithm(dataset, columns, self.k_nearest_neigbours_call, k_index)
-            print('Scores: %s' % scores)
+            f.write("k: " + str(k_index))
+            k_values.append(k_index)
+            scores, precision, recall, f1score = self.evaluate_algorithm(dataset, columns, self.k_nearest_neigbours_call, k_index, w)
+            print_metrics(scores, precision, recall, f1score)
+            accuracy_values.append((sum(scores) / float(len(scores))))
+            f.write("accuracy: " + str((sum(scores) / float(len(scores)))))
+            f.write("\n")
             print('Mean Accuracy: %.3f%%' % (sum(scores) / float(len(scores))))
             print(" --> END of evaluation of KNN algorithm <-- \n")
-            #file.write(str(sum(scores) / float(len(scores))))
 
-        #file.close()
-        
-        # KNN algorithm, k = 10
-        #scores = self.evaluate_algorithm(dataset, self.k_nearest_neighbors, 10)
-        
+        plt.plot(k_values, accuracy_values, label="Dependency of K and accuracy in KNN algorithm")
+        plt.xlabel("Number of neighbours")
+        plt.ylabel("Accuracy metric")
+        plt.show()
+        f.close()
+
+
+        '''
+        # KNN algorithm, k = 110
+
+        y_train = dataset['target'].astype(int)
+        x_train = dataset.drop('target', axis=1)
+        w = self.regularization_coeffients(x_train, y_train.values)
+
+        print(" --> START of evaluation of KNN algorithm <-- \n")
+        scores, precision, recall, f1score = self.evaluate_algorithm(dataset, columns, self.k_nearest_neigbours_call, 110, w)
+        print_metrics(scores, precision, recall, f1score)
+        print(" --> END of evaluation of KNN algorithm <-- \n")
         '''
 
         '''
         # Classification decision tree algorithm
-        # TODO: Check what are the best parameters for this algorithm
         min_samples = 2
         max_depth = 5
+        metric_function = calculate_entropy
 
         print(" --> START of evaluation of classification decision tree algorithm <-- \n")
         dataset['target'] = pd.to_numeric(dataset['target'])
-        scores = self.evaluate_algorithm(dataset, columns, self.decision_tree_algorithm_call, min_samples, max_depth)
-        print('Scores: %s' % scores)
-        print('Mean Accuracy: %.3f%%' % (sum(scores) / float(len(scores))))
+        scores, precision, recall, f1score = self.evaluate_algorithm(dataset, columns, self.decision_tree_algorithm_call, min_samples, max_depth, metric_function)
+        print_metrics(scores, precision, recall, f1score)
         print(" --> END of evaluation of classification decision tree algorithm <-- \n")
-        
         '''
 
+
+        '''
+        # Calculation of the best max_depth parameter for classification decision tree
+
+        min_samples = 2
+        max_depth_from = 1
+        max_depth_to = len(columns)-1
+
+        max_depth_values = []
+        accuracy_gini = []
+        accuracy_entropy = []
+        dataset['target'] = pd.to_numeric(dataset['target'])
+
+        print(" --> START of evaluation of classification decision tree algorithm <-- \n")
+
+        print(" --> Evaluation for ENTROPY metric <-- \n")
+        metric_function = calculate_entropy
+        for depth_of_tree in range(max_depth_from, max_depth_to, 2):
+            max_depth_values.append(depth_of_tree)
+            scores, precision, recall, f1score = self.evaluate_algorithm(dataset, columns, self.decision_tree_algorithm_call, min_samples, depth_of_tree, metric_function)
+            print_metrics(scores, precision, recall, f1score)
+            accuracy_entropy.append(sum(scores) / float(len(scores)))
+
+        print(" --> Evaluation for GINI metric <-- \n")
+        metric_function = calculate_gini
+        for depth_of_tree in range(max_depth_from, max_depth_to, 2):
+            scores, precision, recall, f1score = self.evaluate_algorithm(dataset, columns, self.decision_tree_algorithm_call, min_samples, depth_of_tree, metric_function)
+            print_metrics(scores, precision, recall, f1score)
+            accuracy_gini.append(sum(scores) / float(len(scores)))
+
+        print(" --> END of evaluation of classification decision tree algorithm <-- \n")
+
+        plt.plot(max_depth_values, accuracy_gini)
+        plt.plot(max_depth_values, accuracy_entropy)
+        plt.xlabel("Maximum depth of a tree")
+        plt.ylabel("Accuracy metric")
+        plt.show()
+
+        '''
+        '''
         # Classification random forest algorithm
         # TODO: Check how many decision trees to use for this algorithm
 
@@ -141,18 +283,27 @@ class Evaluator:
         number_of_data_in_bootstrap_dataset = 400
         number_of_features = 800
         max_depth = 12
+        metric_function = calculate_entropy
 
         print(" --> START of evaluation of random forest algorithm <-- \n")
         dataset['target'] = pd.to_numeric(dataset['target'])
-        scores = self.evaluate_algorithm(dataset,
+        scores, precision, recall, f1score = self.evaluate_algorithm(dataset,
                                          columns,
                                          self.random_forest_algorithm_call,
                                          number_of_trees,
                                          number_of_data_in_bootstrap_dataset,
                                          number_of_features,
-                                         max_depth)
-        print('Scores: %s' % scores)
-        print('Mean Accuracy: %.3f%%' % (sum(scores) / float(len(scores))))
+                                         max_depth,
+                                         metric_function)
+        print_metrics(scores, precision, recall, f1score)
         print(" --> END of evaluation of random forest algorithm <-- \n")
 
+        '''
 
+        '''
+        # Naive Bayes classifier algorithm
+        print(" --> START of evaluation of Naive Bayes classifier algorithm <-- \n")
+        scores, precision, recall, f1score = self.evaluate_algorithm(dataset, columns, self.naive_bayes_classifier_call)
+        print_metrics(scores, precision, recall, f1score)
+        print(" --> END of evaluation of Naive Bayes classifier algorithm <-- \n")
+        '''
