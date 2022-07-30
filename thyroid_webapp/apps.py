a2c8121d86e09eb8002273c4e573f8a0ee3145b9
dunjadjordjevic <dunjadjordjevic97@gmail.com>
2022-07-30 23:12:19 +0200

    Rename name of app to new one

--- thyroid_webapp/apps.py
+++ thyroid_webapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TyreoidWebappConfig(AppConfig):
+class ThyroidWebappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'thyroid_webapp'
